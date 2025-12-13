72cb135fca11796ea31ca3fab0c260b9e1e3f9ea
RMT <d.rong@outlook.com>
2025-12-13 15:22:32 +0800

    update netflow module

--- nixos/modules/netflow/default.nix
+++ nixos/modules/netflow/default.nix
@@ -1,6 +1,14 @@
-# services order: gost + netflow -> netflow-update -> mosdns
-# workflow: after gost launched, it'd download subcriptions and this process doesn't need outer dns.
-# Then the proxy can be used to download other resources(netflow-update)
+# netflow will split network traffics via destination(inside or outside of china).
+# netflow uses sing-box + nftables for traffic routing and adguardhome + mosdns for dns routing.
+#
+# progress to start netflow:
+# 1. adguardhome should start firstly, and use singbox and other domain specific rules as upstreams.
+# 2. singbox should not dependent other apps to launch, it means singbox has to resolve domains such as node's domain and rulesets's domain in itself
+# 3. nftable's table 'netflow' will direct udp and tcp traffic to singbox's transparent proxy except it in several sets:
+#     reserve4  # reserve ips should not be directed to singbox
+#     reserve6
+#     direct4   # ips from other apps should not be directed to singbox
+#     direct6
 {
   pkgs,
   lib,
@@ -20,7 +28,6 @@ in
   };
   imports = [
     ./secrets.nix
-    ../services/gost.nix
   ];
   config = {
     boot.kernelModules = [
@@ -58,115 +65,13 @@ in
         ip -6 r add local ::/0 dev lo table 200 || true
 
         # create filter lists
-        if [ ! -e "${stateDir}/chnlist.txt" ]; then
-          touch "${stateDir}/chnlist.txt"
-        fi
-        cp ${./sets/reserve_domains.txt} "${stateDir}/reserve_domains.txt"
+        # if [ ! -e "${stateDir}/chnlist.txt" ]; then
+        #   touch "${stateDir}/chnlist.txt"
+        # fi
+        # cp ${./sets/reserve_domains.txt} "${stateDir}/reserve_domains.txt"
       '';
       serviceConfig = {
         Type = "oneshot";
-      };
-    };
-
-    systemd.services.netflow-update = {
-      after = [
-        "netflow.service"
-        "network-online.service"
-        "gost.service"
-        "nftables.service"
-      ];
-      wantedBy = [ "multi-user.target" ];
-      path = [
-        pkgs.python3
-        pkgs.curl
-        pkgs.nftables
-      ];
-      environment = {
-        ALL_PROXY = "socks5://127.0.0.1:7891";
-        HTTP_PROXY = "socks5://127.0.0.1:7891";
-        HTTPS_PROXY = "socks5://127.0.0.1:7891";
-      };
-      script = ''
-        # when host reboot, it will load previous data
-        if [ -e ${stateDir}/ipv4_list.nft ]; then
-          nft -f ${stateDir}/ipv4_list.nft
-        fi
-        if [ -e ${stateDir}/ipv6_list.nft ]; then
-          nft -f ${stateDir}/ipv6_list.nft
-        fi
-
-        until curl -I -s https://www.google.com >/dev/null
-        do
-          echo "wait to that i can visit internet..."
-          sleep 10
-        done
-
-        # update domain list
-        curl -s -f -m 30 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf' -o ${stateDir}/chnlist.txt.new
-        if [ "$?" = "0" ]; then
-          python ${./convert.py} domain ${stateDir}/chnlist.txt.new ${stateDir}/chnlist.txt.new
-
-          old_md5=$(md5sum ${stateDir}/chnlist.txt)
-          new_md5=$(md5sum ${stateDir}/chnlist.txt.new)
-
-          if [ "$old_md5" != "$new_md5" ]; then
-            mv ${stateDir}/chnlist.txt.new ${stateDir}/chnlist.txt
-          fi
-        fi
-
-        # update ip list
-        curl -s -f -m 30 'https://raw.githubusercontent.com/fernvenue/chn-cidr-list/master/ipv4.txt' -o ${stateDir}/ipv4_list.txt
-        if [ "$?" = "0" ]; then
-          python ${./convert.py} ip --set 'inet netflow direct4' ${stateDir}/ipv4_list.txt ${stateDir}/ipv4_list.nft
-          nft -f ${stateDir}/ipv4_list.nft
-        fi
-
-
-        curl -s -f -m 30 'https://raw.githubusercontent.com/fernvenue/chn-cidr-list/master/ipv6.txt' -o ${stateDir}/ipv6_list.txt
-        if [ "$?" = "0" ]; then
-          python ${./convert.py} ip --set 'inet netflow direct6' ${stateDir}/ipv6_list.txt ${stateDir}/ipv6_list.nft
-          nft -f ${stateDir}/ipv6_list.nft
-        fi
-      '';
-      serviceConfig = {
-        Type = "oneshot";
-      };
-    };
-    systemd.timers.netflow-update = {
-      after = [
-        "netflow.service"
-        "network-online.service"
-        "gost.service"
-      ];
-      wantedBy = [ "multi-user.target" ];
-      timerConfig = {
-        OnCalendar = "daily";
-        Unit = "netflow-update.service";
-      };
-    };
-
-    # mosdns needs chnlist for work, so wait until netflow-update completing
-    systemd.services.mosdns = {
-      after = [
-        "netflow.service"
-        "gost.service"
-      ];
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.mosdns ];
-      script = "mosdns start -c ${./mosdns.yaml}";
-      serviceConfig = {
-        Type = "simple";
-      };
-    };
-    systemd.paths.chnlist = {
-      after = [
-        "gost.service"
-        "netflow.service"
-        "netflow-update.service"
-      ];
-      pathConfig = {
-        PathChanged = "${stateDir}";
-        Unit = "mosdns.service";
       };
     };
 
