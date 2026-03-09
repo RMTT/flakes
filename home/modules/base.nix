b1af13629ba46526ee458064c1879556d071d399
RMT <iamrmttt@gmail.com>
2026-03-09 15:13:57 +0800

    add garnix cache

--- home/modules/base.nix
+++ home/modules/base.nix
@@ -15,10 +15,12 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://colmena.cachix.org"
+      "https://cache.garnix.io"
     ];
     extra-trusted-public-keys = [
       "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
     experimental-features = [
       "nix-command"
--- nixos/modules/base.nix
+++ nixos/modules/base.nix
@@ -27,9 +27,11 @@ with lib;
     nix.settings.substituters = [
       "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
+      "https://cache.garnix.io"
     ];
     nix.settings.trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
     nix.settings.trusted-users = [
       "root"
