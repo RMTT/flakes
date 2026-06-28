6196b2314357cf3d3d4b721f8f50d0c5dc708d63
RMT <iamrmttt@gmail.com>
2026-06-28 17:21:30 +0800

    update neovim mappings

--- home/config/nvim/lua/mapping.lua
+++ home/config/nvim/lua/mapping.lua
@@ -1,28 +1,35 @@
 ---- use alt+hjkl to move between windows ----
-vim.api.nvim_set_keymap('n', '<A-h>', '<C-w>h', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-l>', '<C-w>l', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-j>', '<C-w>j', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-k>', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<A-h>', '<C-w>h', { noremap = true, silent = true, desc = 'Go to left window' })
+vim.keymap.set('n', '<A-l>', '<C-w>l', { noremap = true, silent = true, desc = 'Go to right window' })
+vim.keymap.set('n', '<A-j>', '<C-w>j', { noremap = true, silent = true, desc = 'Go to lower window' })
+vim.keymap.set('n', '<A-k>', '<C-w>k', { noremap = true, silent = true, desc = 'Go to upper window' })
 ---- end ----
 
 -- use alt+pn to move between buffers
-vim.api.nvim_set_keymap('n', '<A-p>', ':bp<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-n>', ':bn<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<A-p>', ':bp<CR>', { noremap = true, silent = true, desc = 'Go to previous buffer' })
+vim.keymap.set('n', '<A-n>', ':bn<CR>', { noremap = true, silent = true, desc = 'Go to next buffer' })
 -- end
 
 ---- using <space>y[y] and <space>p to copy and paster from clipboard ----
-vim.api.nvim_set_keymap('v', '<space>y', '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<space>y', '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<space>Y', '"+yy', { noremap = true, silent = true })
+vim.keymap.set('v', '<space>y', '"+y', { noremap = true, silent = true, desc = 'Copy selection to clipboard' })
+vim.keymap.set('n', '<space>y', '"+y', { noremap = true, silent = true, desc = 'Copy motion to clipboard' })
+vim.keymap.set('n', '<space>Y', '"+yy', { noremap = true, silent = true, desc = 'Copy line to clipboard' })
 
-vim.api.nvim_set_keymap('n', '<space>p', '"+p', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<space>P', '"+P', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<space>p', '"+p', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<space>P', '"+P', { noremap = true, silent = true })
+vim.keymap.set('n', '<space>p', '"+p', { noremap = true, silent = true, desc = 'Paste from clipboard after cursor' })
+vim.keymap.set('n', '<space>P', '"+P', { noremap = true, silent = true, desc = 'Paste from clipboard before cursor' })
+vim.keymap.set('v', '<space>p', '"+p', { noremap = true, silent = true, desc = 'Paste from clipboard after selection' })
+vim.keymap.set('v', '<space>P', '"+P', { noremap = true, silent = true, desc = 'Paste from clipboard before selection' })
 ---- end ----
 
+---- resize window
+vim.keymap.set('n', '<M-=>', ':vertical resize +3<CR>',
+    { noremap = true, silent = true, desc = 'increase width of windows' })
+vim.keymap.set('n', '<M-->', ':vertical resize -3<CR>',
+    { noremap = true, silent = true, desc = 'decrease width of windows' })
+
+
 ---- close buffer and window ----
-vim.api.nvim_set_keymap('n', '<A-w>', ':bd<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<A-w>', ':bd<CR>', { noremap = true, silent = true, desc = 'Close buffer' })
 
 
 -- copy location inside file
