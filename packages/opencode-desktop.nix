b29b61d66362c7af5ab423345bad69c38536b84f
RMT <iamrmttt@gmail.com>
2026-07-14 14:16:31 +0800

    update hash of opencode-desktop

--- packages/opencode-desktop.nix
+++ packages/opencode-desktop.nix
@@ -13,7 +13,7 @@ let
     inherit version src;
     node_modules = old.node_modules.overrideAttrs (oldModules: {
       inherit version src;
-      outputHash = "sha256-kXdXw264JQdlNoZPv5GUyWZvb/A8h2CTRdiX79jyvys=";
+      outputHash = "sha256-3NAzmlzVBcLSRXxpNOyW5DKfD1i2HReST2jlKgrtOKc=";
     });
   });
 in
