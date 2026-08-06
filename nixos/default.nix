ac84351dfc2a784b48084a4fd4062edfc0ca868a
RMT <iamrmttt@gmail.com>
2026-08-06 11:59:01 +0800

    remove cardwire module

--- nixos/default.nix
+++ nixos/default.nix
@@ -13,7 +13,6 @@ let
     inputs.home-manager.nixosModules.home-manager
     inputs.disko.nixosModules.disko
     inputs.determinate.nixosModules.default
-    inputs.cardwire.nixosModules.default
     (
       { config, lib, ... }:
       {
