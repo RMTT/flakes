ea1c555696bdf4545ac56e38f8bf434978eae333
RMT <iamrmttt@gmail.com>
2026-09-14 23:23:26 +0800

    update

--- home/default.nix
+++ home/default.nix
@@ -4,31 +4,36 @@
   ...
 }@inputs:
 let
-  overlay-ownpkgs = platform: final: prev: inputs.self.packages.${platform};
+  overlay-ownpkgs =
+    platform: final: prev:
+    inputs.self.packages.${platform};
 in
 {
-  rmt = let platform = "aarch64-darwin";
-  in home-manager.lib.homeManagerConfiguration {
-    pkgs = import nixpkgs-fresh {
-      system = platform;
-      config.allowUnfree = true;
+  rmt =
+    let
+      platform = "aarch64-darwin";
+    in
+    home-manager.lib.homeManagerConfiguration {
+      pkgs = import nixpkgs-fresh {
+        system = platform;
+        config.allowUnfree = true;
+      };
+      modules = [
+        {
+          nixpkgs.overlays = [
+            (overlay-ownpkgs platform)
+          ];
+          programs.home-manager.enable = true;
+        }
+        inputs.nur.modules.homeManager.default
+        inputs.sops-nix.homeManagerModules.sops
+        {
+          home.username = "rmt";
+          home.homeDirectory = "/Users/rmt";
+          home.stateVersion = "26.11";
+        }
+        ./modules/base.nix
+        ./modules/darwin
+      ];
     };
-    modules = [
-      {
-        nixpkgs.overlays = [
-           (overlay-ownpkgs platform)
-        ];
-        programs.home-manager.enable = true;
-      }
-      inputs.nur.modules.homeManager.default
-      inputs.sops-nix.homeManagerModules.sops
-      {
-        home.username = "rmt";
-        home.homeDirectory =  "/Users/rmt" ;
-        home.stateVersion = "26.11";
-      }
-      ./modules/base.nix
-      ./modules/darwin
-    ];
-  };
 }
--- packages/default.nix
+++ packages/default.nix
@@ -7,6 +7,5 @@ let
   };
 in
 {
-  sblite = pkgs.callPackage ./sblite.nix { };
   virtme-ng = pkgs.callPackage ./virtme-ng { };
 }
